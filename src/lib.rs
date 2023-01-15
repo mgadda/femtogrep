25fef690ccd51d0b119e2cc2c9b9918216f8a066
Matt Gadda <mgadda@gmail.com>
2023-01-14 21:13:32 -0800

    support recursive search

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,46 @@
+use std::collections::VecDeque;
 use std::error::Error;
 use std::fs;
+use std::fs::DirEntry;
+use std::path::Path;
 use std::result::Result;
 use std::str::RMatchIndices;
 use termion::color;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-  config
-    .file_paths
-    .iter()
-    .map(|file_path| run_one(config.query, file_path))
-    .collect()
+  // let mut paths_to_process: Vec<&str> = config.file_paths.iter().map(|f| f.as_str()).collect();
+  // let mut paths_to_process: Vec<String> = Vec::from(config.file_paths);
+
+  let mut paths_to_process: VecDeque<String> = VecDeque::new();
+  paths_to_process.extend(config.file_paths.iter().map(String::clone));
+
+  while let Some(file_path) = paths_to_process.pop_front() {
+    // extracting actual paths from read_dir is extraordinarily painful
+    // on account of it returning a Result<ReadDir<Result<DirEntry>>>
+    let path = Path::new(&file_path);
+    if path.is_dir() && config.recursive {
+      if let Ok(result) = fs::read_dir(&file_path) {
+        let new_paths: Vec<String> = result
+          .filter_map(|dir_entry_result| -> Option<String> {
+            dir_entry_result.map_or(None, |dir_entry: DirEntry| -> Option<String> {
+              dir_entry.path().to_str().map(|f| f.to_string())
+            })
+          })
+          .collect();
+
+        // can this be replaced with `extend`?
+        for path in new_paths {
+          paths_to_process.push_back(path);
+        }
+      }
+
+      // paths_to_process.extend()
+    } else if path.is_file() {
+      let _ = run_one(config.query, &file_path);
+    }
+  }
+
+  Ok(())
 }
 
 pub fn run_one(query: &str, file_path: &str) -> Result<(), Box<dyn Error>> {
@@ -18,11 +49,7 @@ pub fn run_one(query: &str, file_path: &str) -> Result<(), Box<dyn Error>> {
   let red = color::Fg(color::Red);
   let reset = color::Fg(color::Reset);
 
-  println!(
-    "{}{file_path}{}:",
-    color::Fg(color::Blue),
-    color::Fg(color::Reset)
-  );
+  let mut found_matches = false;
 
   for line in search(query, &contents) {
     let mut formatted_line = String::from(line.0);
@@ -33,6 +60,15 @@ pub fn run_one(query: &str, file_path: &str) -> Result<(), Box<dyn Error>> {
       formatted_line.insert_str(start, &format!("{}", red));
     }
 
+    if !found_matches {
+      // only print out the filename if we actually have found at least one match
+      println!(
+        "\n{}{file_path}{}:",
+        color::Fg(color::Blue),
+        color::Fg(color::Reset)
+      );
+      found_matches = true;
+    }
     println!("  {formatted_line}");
   }
 
@@ -46,6 +82,7 @@ pub struct Config<'a> {
   // that live for the duration of the program.
   pub query: &'a str,
   pub file_paths: &'a [String],
+  pub recursive: bool,
 }
 
 impl<'a> Config<'a> {
@@ -57,22 +94,27 @@ impl<'a> Config<'a> {
     let query = &args[1]; //.clone();
     let file_paths = &args[2..]; //.clone();
 
-    Ok(Config { query, file_paths })
+    Ok(Config {
+      query,
+      file_paths,
+      recursive: true,
+    })
   }
 }
 
 pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<(&'a str, RMatchIndices<'a, &'a str>)> {
   // 'a lifetime var connects the lifetime of contents to the return value
   // since the return value is a list of str references from contents
-  let mut matches: Vec<(&str, RMatchIndices<&str>)> = Vec::new();
-
-  for line in contents.split("\n") {
-    if line.contains(query) {
-      matches.push((line, line.rmatch_indices(query)));
-    }
-  }
-
-  matches
+  contents
+    .split("\n")
+    .filter_map(|line| {
+      if line.contains(query) {
+        Some((line, line.rmatch_indices(query)))
+      } else {
+        None
+      }
+    })
+    .collect()
 }
 
 #[cfg(test)]
@@ -81,13 +123,13 @@ mod tests {
 
   #[test]
   fn one_result() {
-    let query = "duct";
+    let query = "Pick";
     let contents = "\
 Rust
 safe, fast, productive.
 Pick three.
     ";
 
-    assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    assert_eq!("Pick", search(query, contents)[0].0);
   }
 }
